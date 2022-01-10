3da8779f3496537dc4461d07eba2f5e669c8f5c6
EvgenyPlaksin <plaksinevgeniy766@gmail.com>
2022-01-10 16:02:03 +0200

    final commit

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -6,6 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.weatherapp.databinding.ActivityMainBinding
 import com.example.weatherapp.viewmodel.WeatherViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -19,11 +23,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val tomorrow = LocalDate.now().plus(1, ChronoUnit.DAYS)
+        val formattedTomorrow = tomorrow.format(DateTimeFormatter.ofPattern("EE, MMM d"))
+
+        val tomorrow1 = LocalDate.now().plus(2, ChronoUnit.DAYS)
+        val formattedTomorrow1 = tomorrow1.format(DateTimeFormatter.ofPattern("EE, MMM d"))
+
+        val tomorrow2 = LocalDate.now().plus(3, ChronoUnit.DAYS)
+        val formattedTomorrow2 = tomorrow2.format(DateTimeFormatter.ofPattern("EE, MMM d"))
 
         viewModel.weatherResponse.observe(this, { weather ->
 
             binding.apply {
-                tvCityName.text = "Kiev"
+                tvCityName.text = "London"
                 tvDescription.text = weather.description
                 tvTemperature.text = weather.temperature
                 tvWind.text = weather.wind
@@ -33,6 +45,9 @@ class MainActivity : AppCompatActivity() {
                 tvForecast2.text = "${forecast[1].temperature}/ ${forecast[1].wind}"
                 tvForecast3.text = "${forecast[2].temperature}/ ${forecast[2].wind}"
 
+                day1.text = formattedTomorrow
+                day2.text = formattedTomorrow1
+                day3.text = formattedTomorrow2
             }
 
         })
